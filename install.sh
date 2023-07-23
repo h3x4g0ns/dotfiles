152229492abaa137a0d75f6c0fa0890fe96d414c
h3x4g0ns <agrawalekansh02@gmail.com>
2023-07-23 14:06:13 -0700

    removed stow

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
 sysname=$(uname -s)
-packages="bpytop tmux neovim alacritty nvtop wget stow tree starship neofetch htop"
+packages="bpytop tmux alacritty wget tree neofetch htop ripgrep ncdu speedtest-cli make cmake tmux"
 
 if [[ "$sysname" == "Darwin" ]]; then
-  if command -v brew >/dev/null 2>&1; then
+  if command -v brew &>/dev/null; then
+    echo "Homebrew is installed."
   else
     echo "Homebrew is not installed."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -12,15 +12,34 @@ if [[ "$sysname" == "Darwin" ]]; then
 
   # installing brew packages
   brew install $packages
+
+  # custom commands
+  echo "source ~/.custom.sh" >> ~/.zshrc
 else
   # installing ubuntu packages
   sudo apt-get update && sudo apt-get upgrade
-  sudo apt-get install $packages
+  sudo apt-get install -y $packages
+  sudo apt-get install -y nvtop
+
+  # custom commands
+  echo "source ~/.custom.sh" >> ~/.bashrc
 fi
 
-# platform agnostic
-curl -sS https://starship.rs/install.sh | sh
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+# installing neovim from source
+if command -v nvim &>/dev/null; then
+  echo "Neovim is installed."
+else
+  echo "Neovim is not installed"
+  mkdir -p ~/Code && cd ~/Code
+  git clone https://github.com/neovim/neovim.git
+  cd neovim
+  make CMAKE_BUILD_TYPE=RelWithDebInfo
+  sudo make install
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+fi
 
-# stowing config files
-stow -d . -t ~ home
+# installing starship prompt
+curl -sS https://starship.rs/install.sh | sh
+
+# applying custom config files
+mv ~/dotfiles/home/* ~/.
