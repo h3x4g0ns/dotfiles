d7508567e40bf7cfd5e309e2af93872e710a495e
h3x4g0ns <agrawalekansh02@gmail.com>
2025-10-12 14:14:43 -0700

    adding yazi quick switch function

--- home/.custom.sh
+++ home/.custom.sh
@@ -70,6 +70,14 @@ function condainit {
 # <<< conda initialize <<<
 }
 
+function y {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
 # prompt
 eval "$(zoxide init zsh)"
 eval "$(starship init zsh)"
