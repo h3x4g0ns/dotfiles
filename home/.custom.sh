100954c103df1c16bc545228f85e644bcced8b53
h3x4g0ns <agrawalekansh02@gmail.com>
2026-01-07 21:02:52 -0800

    simplifying alacritty colors

--- home/.custom.sh
+++ home/.custom.sh
@@ -55,19 +55,19 @@ function switch {
 }
 
 function condainit {
-# # !! Contents within this block are managed by 'conda init' !!
-  __conda_setup="$('/home/hexagon/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+  # # !! Contents within this block are managed by 'conda init' !!
+  __conda_setup="$('/Users/hexagon/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
   if [ $? -eq 0 ]; then
       eval "$__conda_setup"
   else
-      if [ -f "/home/hexagon/miniconda3/etc/profile.d/conda.sh" ]; then
-          . "/home/hexagon/miniconda3/etc/profile.d/conda.sh"
+      if [ -f "/Users/hexagon/miniconda3/etc/profile.d/conda.sh" ]; then
+          . "/Users/hexagon/miniconda3/etc/profile.d/conda.sh"
       else
-          export PATH="/home/hexagon/miniconda3/bin:$PATH"
+          export PATH="/Users/hexagon/miniconda3/bin:$PATH"
       fi
   fi
   unset __conda_setup
-# <<< conda initialize <<<
+  # <<< conda initialize <<<
 }
 
 function y {
